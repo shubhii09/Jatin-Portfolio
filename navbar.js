664736c46fbe0eac2377809c300626c61347123c
Shubham <shubhchauhan.dev@gmail.com>
2025-09-04 00:54:04 +0530

    creating more page and more

---
+++ navbar.js
@@ -0,0 +1,13 @@
+// js/navbar.js
+document.addEventListener("DOMContentLoaded", () => {
+  const navbarPlaceholder = document.getElementById("navbar-placeholder");
+
+  if (navbarPlaceholder) {
+    fetch("navbar.html") // loads your navbar file
+      .then(response => response.text())
+      .then(data => {
+        navbarPlaceholder.innerHTML = data;
+      })
+      .catch(error => console.error("Error loading navbar:", error));
+  }
+});
